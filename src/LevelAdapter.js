962b882c4170b386832bafe7e79f4b41521d4ef7
Greg Linklater <greglink49@gmail.com>
2019-05-16 11:54:10 +0200

    refactor(LevelAdapter): rename param

--- src/LevelAdapter.js
+++ src/LevelAdapter.js
@@ -61,19 +61,24 @@ class LevelAdapter {
     }
   }
 
-  async search (gte = '', options = {}) {
-    if (typeof gte === 'object') {
-      options = gte
-      gte = options.gte || options.lte || options.search
+  async search (startsWith = '', options = {}) {
+    if (typeof startsWith === 'object') {
+      options = startsWith
+      startsWith = options.startsWith || options.gte || options.search
     }
 
-    const { limit = -1, reverse = false } = options
+    const { limit = -1, reverse = false, lte } = options
 
     return await new Promise((resolve, reject) => {
       const data = {}
 
-      // Get all values that start with `gte`
-      this.db.createReadStream({ gte, lte: `${gte}\uffff`, limit, reverse })
+      // Get all values that start with `startsWith`
+      this.db.createReadStream({
+        gte: startsWith,
+        lte: lte ? lte : `${startsWith}\uffff`,
+        limit,
+        reverse,
+      })
         .on('data', item => data[item.key] = item.value)
         .on('end', () => resolve(data))
         .on('error', err => reject(err))
